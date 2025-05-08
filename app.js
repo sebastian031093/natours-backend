29e4176475fad836ff8d854bbb48538483f9466d
Sebastian Manco G <sebasmg1393@gmail.com>
2025-05-08 18:23:52 -0500

    sebastian manco: spit code started

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 console.log('Hello from my backend server app.');
 
@@ -7,7 +8,17 @@ const app = express();
 
 //TODO: Middlewere: is a basically a function that can modify the incoming request data. "middle into request and response"
 app.use(express.json());
+app.use(morgan('dev'));
 
+app.use((req, resp, next) => {
+  console.log('Hello from the middlewere.');
+  next();
+});
+
+app.use((req, resp, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 // app.get('/', function (req, resp) {
 //   // resp.status(200).send('Hello from expresss server active.');
 //   resp.status(200).json({
@@ -24,17 +35,18 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 
-app.get('/api/v1/tours', function (req, resp) {
+const getAllTours = function (req, resp) {
   resp.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', function (req, resp) {
+const createTour = function (req, resp) {
   console.log(req.body);
 
   const newID = tours[tours.length - 1].id + 1;
@@ -57,9 +69,9 @@ app.post('/api/v1/tours', function (req, resp) {
   );
 
   // resp.send('Done from POST');
-});
+};
 
-app.get('/api/v1/tours/:id', function (req, resp) {
+const getTour = function (req, resp) {
   console.log(req.params);
   const id = Number(req.params.id);
 
@@ -78,9 +90,9 @@ app.get('/api/v1/tours/:id', function (req, resp) {
       tour,
     },
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', function (req, resp) {
+const updatedTour = function (req, resp) {
   const id = Number(req.params.id);
 
   if (id > tours.length) {
@@ -96,7 +108,73 @@ app.patch('/api/v1/tours/:id', function (req, resp) {
       tour: '<Update tour>',
     },
   });
-});
+};
+
+const deletedTour = (req, resp) => {
+  const id = Number(req.params.id);
+
+  if (id > tours.length) {
+    return resp.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  resp.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+const getAllUsers = (req, resp) => {
+  resp.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const createdUser = (req, resp) => {
+  resp.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, resp) => {
+  resp.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const updateddUser = (req, resp) => {
+  resp.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const deletedUser = (req, resp) => {
+  resp.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updatedTour);
+// app.delete('/api/v1/tours/:id', deletedTour);
+
+//3) ROUTE
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updatedTour).delete(deletedTour);
+
+userRouter.route('/').get(getAllUsers).post(createdUser);
+userRouter.route('/:id').get(getUser).patch(updateddUser).delete(deletedUser);
 
 const PORT = 3000;
 
