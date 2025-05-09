59a071d5cc4e1ae673a6897000b08d93bd39f358
Sebastian Manco G <sebasmg1393@gmail.com>
2025-05-09 09:42:27 -0500

    sebas:start db connection

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const dotenv = require('dotenv');
 const app = require('./app');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 // console.log(app.get('env'));
-console.log(process.env);
+// console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.BD_PASSWORD);
+// console.log('db', DB);
+
+mongoose.connect();
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(con => {
+    console.log(con.connections);
+    console.log('DB connection succefully.');
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
