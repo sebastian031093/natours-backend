08a8d15e7370f73e37cfbf9039c4699b53ccd592
Sebastian Manco G <sebasmg1393@gmail.com>
2025-05-10 19:12:36 -0500

    sebas: DB connected

--- server.js
+++ server.js
@@ -5,20 +5,54 @@ const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 // console.log(app.get('env'));
 // console.log(process.env);
-
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.BD_PASSWORD);
-// console.log('db', DB);
-
-mongoose.connect();
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(con => {
-    console.log(con.connections);
-    console.log('DB connection succefully.');
+    // console.log(con.connections);
+    // console.log('DB connection succefully.');
+  });
+
+//TODO: schema to describe out data, to set dafault values, to validate the data and all kind of stuff like that.
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour mus have a price'],
+  },
+});
+
+//TODO:MODEL: BLUE PRINT THAT WE USE TO CREATE MODELS "TABLES" LIKE A CLASSES IN JS AND OLSO TO QUERY, UPDATE AND DELETE THESE DOCUEMTNS.
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forrest Hiker',
+  rating: 4.7,
+  price: 654,
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log('doc', doc);
+  })
+  .catch(error => {
+    console.log('error from', error);
   });
 
 const PORT = process.env.PORT || 3000;
